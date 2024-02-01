5e64143544ac2d3fdcc67a346a192ba56ce18fce
Damian Hallengren <djhallengren@gmail.com>
2024-02-01 15:52:49 +0100

    initial commit + header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import logoW from "./assets/logo-white.svg"
+
 
 function App() {
+  // Reusable button for various points in the app
+  function Button({href, buttonClass, text="Button Button"}) {
+    return (
+      <a href={href} className={"button " + buttonClass}>
+        {text}
+      </a>
+    )
+  }
+
+
+  function Header({}) {
+
+    return (
+      <div id="header">
+        <img src={logoW} alt="Rooster Grin Logo" className="header-logo"/>
+        <div className="header-content">
+          <h1 className="title-header">
+            <span className='upper-title'>Welcome To</span><br />
+            Rooster Grin
+          </h1>
+          <Button href="/" buttonClass="button-main"/>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }

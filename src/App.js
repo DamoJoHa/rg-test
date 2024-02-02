383ac1c467e9e44b97378ab355e8aca23832ee83
Damian Hallengren <djhallengren@gmail.com>
2024-02-02 20:31:16 +0100

    finished page, now with beating animation to indicate active image in grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import {useState} from "react"
-
 import './styles/App.scss';
 import logoW from "./assets/logo-white.svg"
 import logoC from "./assets/logo-color.svg"
@@ -10,7 +8,12 @@ import expertise from "./assets/home-expertise.svg"
 import hygiene from "./assets/home-hygiene.svg"
 import lab from "./assets/home-lab.svg"
 import retention from "./assets/home-retention.svg"
+
 import React from 'react';
+import {useState} from "react"
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 
 function App() {
@@ -18,17 +21,19 @@ function App() {
   const sampleTextShort = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   const sampleTitle = "Lorem Ipsum"
   const sampleLongTitle = "Lorem Ipsum Dolor Sit Amet"
+
+
   // Reusable button for various points in the app
-  function Button({href, buttonClass, text="Button Button"}) {
+  function Button({href="#header", target="", buttonClass, text="Button Button"}) {
     return (
-      <a href={href} className={"button " + buttonClass}>
+      <a href={href} target={target} className={"button " + buttonClass} rel="noreferrer">
         {text}
       </a>
     )
   }
 
 
-  function Header({}) {
+  function Header() {
     return (
       <div id="header">
         <Logo />
@@ -37,21 +42,21 @@ function App() {
             <span className='upper-title'>Welcome To</span><br />
             Rooster Grin
           </h1>
-          <Button href="/" buttonClass="button-main"/>
+          <Button href="https://github.com/roostergrin" target="_blank" buttonClass="button-main"/>
         </div>
       </div>
     )
   }
 
-  function Footer({}) {
+  function Footer() {
     return (
       <div id="footer" className="background-grey">
         <h2>Follow Us:</h2>
         <div>
-          <a href="https://www.instagram.com/roostergrinmedia/" target="_blank">
+          <a href="https://www.instagram.com/roostergrinmedia/" target="_blank" rel="noreferrer">
             <img src={instaLogo} alt="instagram logo" class="footer-logo" />
           </a>
-          <a href="https://www.facebook.com/roostergrinmedia/" target="_blank">
+          <a href="https://www.facebook.com/RoosterGrin/" target="_blank" rel="noreferrer">
             <img src={fbLogo} alt="facebook logo" class="footer-logo" />
           </a>
         </div>
@@ -59,21 +64,30 @@ function App() {
     )
   }
 
-  function Logo({}) {
+  function Logo() {
     return (
-      <a href='https://www.roostergrin.com/' target="_blank">
+      <a href='https://www.roostergrin.com/' target="_blank"  rel="noreferrer">
         <img src={logoW} alt="Rooster Grin Logo" id="logo-white" className="header-logo"/>
         <img src={logoC} alt="Rooster Grin Logo" id="logo-color" className="header-logo"/>
       </a>
     )
   }
 
-  function ImageGrid({}) {
+  function ImageGrid() {
+    const images = document.getElementsByClassName("image-grid-image")
     const [titleText, setTitleText] = useState("")
 
     function focus(event) {
       const text = event.target.getAttribute("alt")
       setTitleText(text)
+
+      // This is necessary because of the way HTML collections are.
+      // Could definitely make this easier on the eyes if we passed the imgs to
+      // this component as a prop, but
+      for (let i = 0; i < images.length; i++ ) {
+        images[i].setAttribute("class", "image-grid-image")
+      }
+      event.target.setAttribute("class", "image-grid-image beating")
     }
 
     return (
@@ -87,6 +101,24 @@ function App() {
     )
   }
 
+  function TextCarousel() {
+    return (
+      <Carousel showThumbs={false} showArrows={true} autoPlay={true} dynamicHeight={false} showStatus={false} infiniteLoop={true}>
+        <div>
+          <p>{sampleText}</p>
+        </div>
+        <div>
+          <p>{sampleTextShort}</p>
+        </div>
+        <div>
+          <p>
+            This is a piece of sample text that I'm writing because I need three things, but don't want to just use a different lenght of "Lorem Ipsum" again.
+          </p>
+        </div>
+      </Carousel>
+    )
+  }
+
   return (
     <div className="App">
       <Header />
@@ -97,7 +129,7 @@ function App() {
             <div className="text-box">
               <h2>{sampleTitle}</h2>
               <p>{sampleText}</p>
-              <a href="/" target='_blank' className="link">> {sampleTitle}</a>
+              <a href="/" target='_blank' className="link" rel="noreferrer">> {sampleTitle}</a>
             </div>
           </div>
           <img src={climberImage} alt="Two climbers helping each other up a boulder." id="climber-image"/>
@@ -115,7 +147,7 @@ function App() {
             <div className="text-box">
               <h2 className='grey-text'>{sampleLongTitle}</h2>
               <p>{sampleTextShort}</p>
-              <a href="/" target="_blank" className='link'>> {sampleTitle}</a>
+              <a href="/" target="_blank" className='link' rel="noreferrer">> {sampleTitle}</a>
             </div>
           </div>
           <div id="image-grid-holder" className="background-grey">
@@ -145,7 +177,7 @@ function App() {
             </div>
           </div>
           <div className="split-right">
-
+            <TextCarousel />
           </div>
         </div>
       </div>
